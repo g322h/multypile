7cb6bd0e452f498d8b24b4218f5dab6ca19914aa
g322h <play02s@mail.ru>
2021-05-14 17:00:57 +0300

    added section slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
-$(function () {
+$(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: '17px',
+        readOnly: true
+    });
 
+    $('.slider__inner-items').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.png" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.png" alt=""></button>'
+    });
     
 });
